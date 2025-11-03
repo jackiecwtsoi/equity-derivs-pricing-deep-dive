1726a22ba79d13030d7d15fb0a0b0a38c32d537a
Jackie <jackiecwtsoi@gmail.com>
2025-11-03 23:20:37 +0800

    Add multi-step MC for European options.

--- src/pricing/monte_carlo.py
+++ src/pricing/monte_carlo.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+import numpy as np
 from jax import random
 from .base import BaseModel
 
@@ -14,20 +15,20 @@ class MonteCarloModel(BaseModel):
 
     def price(self, S, K, T, r, sigma, q, option_type) -> float:
         # 1. Generate random price paths
-        # TODO - Need to fix this to calculate multi-step scenario also
-        prices = self.__generate_risk_neutral_price_paths_single_step(S, T, r, sigma, q)
-        print(prices)
+        price_matrix = self.__generate_risk_neutral_price_paths_multi_step(S, T, r, sigma, q, 100)
+        prices = price_matrix[:, -1] # Last column for European options (FIXME)
+
+        # prices = self.__generate_risk_neutral_price_paths_single_step(S, T, r, sigma, q)
 
         # 2. Calculate option payoff for each path
         payoffs = jnp.maximum(prices - K, 0)
-        print(payoffs)
 
         # 3. Average all payoffs and discount to present value
         option_price = jnp.exp(-r * T) * jnp.mean(payoffs)
 
         return option_price
     
-    
+    @DeprecationWarning
     def __generate_risk_neutral_price_paths_single_step(self, S0, T, r, sigma, q):
         # 1. Generate standard normal random shocks (effectively z(t))
         Z = random.normal(random.PRNGKey(0), shape=(self.num_simulations,))
@@ -36,3 +37,19 @@ class MonteCarloModel(BaseModel):
         final_prices_at_maturity = S0 * jnp.exp((r - q - 0.5 * sigma**2) * T + sigma * jnp.sqrt(T) * Z)
 
         return final_prices_at_maturity
+
+    def __generate_risk_neutral_price_paths_multi_step(self, S0, T, r, sigma, q, num_steps):
+        dt = T / num_steps
+
+        # 1. Initialize price matrix of dimension num_simulations * (num_steps + 1)
+        price_matrix = np.zeros((self.num_simulations, num_steps + 1))
+        price_matrix[:, 0] = S0 # All paths start at S0
+
+        # 2. Generate standard normal random shocks for all steps
+        Z = np.random.standard_normal((self.num_simulations, num_steps))
+
+        # 3. Simulate prices at each time step
+        for t in range(1, num_steps + 1):
+            price_matrix[:, t] = price_matrix[:, t-1] * np.exp((r - q - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * Z[:, t-1])
+        
+        return price_matrix
